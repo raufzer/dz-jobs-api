3278633cc0990927a5d45ed6fe5052b699a6adb3
raufzer <a_zerkhef@estin.dz>
2024-11-23 17:55:27 +0100

    refactor: organize code structure Organize code structure into internal packages

---
+++ data/response/user_response.go
@@ -0,0 +1,19 @@
+package response
+
+import (
+	"time"
+)
+
+type UserResponse struct {
+	ID        uint      `json:"id"` 
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Role      string    `json:"role,omitempty"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type LoginResponse struct {
+	TokenType string `json:"token_type"`
+	Token     string `json:"token"`
+}
