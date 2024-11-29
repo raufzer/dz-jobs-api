07013da35baf8e3aa6d4536238c566807dbf3811
raufzer <a_zerkhef@estin.dz>
2024-11-29 15:05:07 +0100

    feat(database): enhance database connection handling

--- config/database.go
+++ config/database.go
@@ -4,22 +4,19 @@ import (
 	"database/sql"
 	"log"
 
-	_ "github.com/lib/pq" // PostgreSQL driver
+	_ "github.com/lib/pq"
 )
 
 type DatabaseConfig struct {
 	DB *sql.DB
 }
 
-// ConnectDatabase initializes and connects to the PostgreSQL database using raw SQL.
 func ConnectDatabase(config *AppConfig) *DatabaseConfig {
-	// Open a connection to PostgreSQL
 	db, err := sql.Open("postgres", config.DatabaseURI)
 	if err != nil {
 		log.Fatalf("Error while opening connection to PostgreSQL: %v", err)
 	}
 
-	// Test the connection
 	if err := db.Ping(); err != nil {
 		log.Fatalf("Error while connecting to PostgreSQL: %v", err)
 	}
