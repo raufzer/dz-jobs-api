188f055c0c8a952d9be0300d2cfa3fa512fe8c52
raufzer <a_zerkhef@estin.dz>
2024-12-18 01:56:38 +0100

    docs: adjust host url

--- docs/docs.go
+++ docs/docs.go
@@ -405,7 +405,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "https://dz-jobs-api-production.up.railway.app",
+	Host:             "dz-jobs-api-production.up.railway.app",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "DZ Jobs API",
