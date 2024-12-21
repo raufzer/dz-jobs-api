53f0f13223f5c4ad36041c942bb4d620daf2a381
raufzer <a_zerkhef@estin.dz>
2024-12-21 18:00:19 +0100

    feat: change user update method from PATCH to PUT for consistency in user routes

--- internal/routes/api/v1/user_routes.go
+++ internal/routes/api/v1/user_routes.go
@@ -8,9 +8,9 @@ import (
 
 func UserRoutes(rg *gin.RouterGroup, userController *controllers.UserController) {
 	usersRoute := rg.Group("/users")
-	usersRoute.POST("/", userController.CreateUser)      
-	usersRoute.GET("/", userController.GetAllUsers)      
-	usersRoute.GET("/:id", userController.GetUser)       
-	usersRoute.PATCH("/:id", userController.UpdateUser)  
-	usersRoute.DELETE("/:id", userController.DeleteUser) 
+	usersRoute.POST("/", userController.CreateUser)
+	usersRoute.GET("/", userController.GetAllUsers)
+	usersRoute.GET("/:id", userController.GetUser)
+	usersRoute.PUT("/:id", userController.UpdateUser)
+	usersRoute.DELETE("/:id", userController.DeleteUser)
 }
