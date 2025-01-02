a97254c493a3d77ccb1de873b1a680a83b96d13d
raufzer <a_zerkhef@estin.dz>
2025-01-02 13:01:23 +0100

    feat(system-routes): add default route to system routes

--- internal/routes/api/v1/system_routes.go
+++ internal/routes/api/v1/system_routes.go
@@ -8,6 +8,7 @@ import (
 )
 
 func SystemRoutes(rg *gin.RouterGroup, systemController *controllers.SystemController) {
+	rg.GET("/", systemController.DefaultRoute)
 	rg.GET("/version", systemController.GetAPIVersion)
 	rg.GET("/health", systemController.GetHealth)
 	rg.Use(middlewares.MetricsMiddleware())
