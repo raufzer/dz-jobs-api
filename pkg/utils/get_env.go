7efbba6aa1e4fb3f194dee653e8c0db67bdba5f5
raufzer <a_zerkhef@estin.dz>
2025-01-03 10:38:31 +0100

    feat(utils): enhance GetEnv function to support multiple value types

--- pkg/utils/get_env.go
+++ pkg/utils/get_env.go
@@ -3,12 +3,26 @@ package utils
 import (
 	"log"
 	"os"
+	"time"
 )
 
-func GetEnv(key string) string {
+func GetEnv(key string, valueType string) interface{} {
 	value := os.Getenv(key)
 	if value == "" {
 		log.Fatalf("Environment variable %s is not set.", key)
 	}
-	return value
+
+	switch valueType {
+	case "duration":
+		duration, err := time.ParseDuration(value)
+		if err != nil {
+			log.Fatalf("Error parsing duration from environment variable %s: %v", key, err)
+		}
+		return duration
+	case "string":
+		return value
+	default:
+		log.Fatalf("Unsupported value type %s for environment variable %s", valueType, key)
+		return nil
+	}
 }
