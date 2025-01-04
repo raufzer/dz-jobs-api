3223d52c9a699b40179147868b1f316f834ea6fd
raufzer <a_zerkhef@estin.dz>
2025-01-04 11:42:57 +0100

    fix(bookmarks): improve error handling in AddBookmark, RemoveBookmark, and GetBookmarks functions

--- internal/services/bookmarks_service_impl.go
+++ internal/services/bookmarks_service_impl.go
@@ -3,6 +3,8 @@ package services
 import (
 	"dz-jobs-api/internal/models"
 	"dz-jobs-api/internal/repositories/interfaces"
+	"dz-jobs-api/pkg/utils"
+	"net/http"
 
 	"github.com/google/uuid"
 )
@@ -15,15 +17,25 @@ func NewBookmarksService(bookmarksRepo interfaces.BookmarksRepository) *Bookmark
 	return &BookmarksService{bookmarksRepository: bookmarksRepo}
 }
 func (s *BookmarksService) AddBookmark(candidateID uuid.UUID, jobID int64) error {
-	return s.bookmarksRepository.AddBookmark(candidateID, jobID)
+	err := s.bookmarksRepository.AddBookmark(candidateID, jobID)
+	if err != nil {
+		return utils.NewCustomError(http.StatusInternalServerError, "Error adding Bookmark")
+	}
+	return nil
 }
 
 func (s *BookmarksService) RemoveBookmark(candidateID uuid.UUID, jobID int64) error {
-	return s.bookmarksRepository.RemoveBookmark(candidateID, jobID)
+	err := s.bookmarksRepository.RemoveBookmark(candidateID, jobID)
+	if err != nil {
+		return utils.NewCustomError(http.StatusInternalServerError, "Error removing Bookmark")
+	}
+	return nil
 }
 
 func (s *BookmarksService) GetBookmarks(candidateID uuid.UUID) ([]*models.Job, error) {
-	{
-		return s.bookmarksRepository.GetBookmarks(candidateID)
+	bookmarks, err := s.bookmarksRepository.GetBookmarks(candidateID)
+	if err != nil {
+		return nil, utils.NewCustomError(http.StatusInternalServerError, "Error fetching Bookmarks")
 	}
+	return bookmarks, nil
 }
