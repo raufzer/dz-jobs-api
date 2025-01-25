176fd81b1a8a9b2bf5e42025f376e51ea368b5be
raufzer <a_zerkhef@estin.dz>
2025-01-25 20:22:16 +0100

    feat: add custom salary range validation and initialization function

--- internal/helpers/convert_salary_range.go
+++ internal/helpers/convert_salary_range.go
@@ -1,5 +1,12 @@
 package helpers
 
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
+
 func ConvertSalaryRange(min, max float64) (string, []interface{}) {
 	if min > 0 && max > 0 {
 		return `
@@ -15,4 +22,31 @@ func ConvertSalaryRange(min, max float64) (string, []interface{}) {
 			CAST(split_part(salary_range, ' ', 3) AS float) <= $1
 		`, []interface{}{max}
 	}
+}
+// Global validator instance
+var Validate *validator.Validate
+
+func Init() {
+	// Initialize the global validator instance and register custom validation
+	Validate = validator.New()
+	Validate.RegisterValidation("jobSalaryRange", ValidateSalaryRange)
+}
+
+// Custom salary range validator
+func ValidateSalaryRange(fl validator.FieldLevel) bool {
+	// Regex for validating salary range format
+	re := regexp.MustCompile(`^\d{5,}-\d{5,}$`) 
+	if !re.MatchString(fl.Field().String()) {
+		return false
+	}
+
+	// Extract the min and max values from the salary range string
+	var min, max int
+	_, err := fmt.Sscanf(fl.Field().String(), "%d-%d", &min, &max)
+	if err != nil {
+		return false
+	}
+
+	// Ensure that the minimum salary is less than the maximum salary
+	return min < max
 }
